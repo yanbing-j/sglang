e75733a795c0798273c5abefa33f941922bd8223
Jiang, Yanbing <yanbing.jiang@intel.com>
2025-05-12 15:03:24 +0000

    Fix CPU running error

--- python/sglang/srt/layers/moe/fused_moe_triton/layer.py
+++ python/sglang/srt/layers/moe/fused_moe_triton/layer.py
@@ -222,7 +222,11 @@ class UnquantizedFusedMoEMethod(FusedMoEMethodBase, CustomOp):
         num_expert_group: Optional[int] = None,
         custom_routing_function: Optional[Callable] = None,
         correction_bias: Optional[torch.Tensor] = None,
+        activation: str = "silu",
+        apply_router_weight_on_input: bool = False,
         inplace: bool = True,
+        no_combine: bool = False,
+        routed_scaling_factor: Optional[float] = None,
     ) -> torch.Tensor:
         return moe_forward_native(
             layer,
