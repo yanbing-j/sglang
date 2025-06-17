0a550b6f3d2940c86bb464f018587654a7491e4c
Zheng, Beilei <beilei.zheng@intel.com>
2025-06-16 23:27:23 -0700

    Enable FP8 llama3.1-8b

--- python/sglang/srt/layers/quantization/fp8.py
+++ python/sglang/srt/layers/quantization/fp8.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, Tuple
 
 import torch
 import torch.nn.functional as F
@@ -308,6 +308,39 @@ class Fp8Config(QuantizationConfig):
                 dict.fromkeys(hf_to_sglang_mapper.apply_list(self.ignored_layers))
             )
 
+
+def requantize_with_max_scale_cpu(
+        weight: torch.Tensor, weight_scale: torch.Tensor,
+        logical_widths: List[int]) -> Tuple[torch.Tensor, torch.Tensor]:
+    # Max scale to be used for requanitzation.
+    max_w_scale = weight_scale.max()
+
+    # QKV / MLP is fused in the on disk checkpoint if any of the
+    # weight scales are still set to the default since we initialize
+    # N weight scales for N shards but we only load 1 weight scale
+    # from disk in this case. Skip requantization in this case (since)
+    # we already are quantized with the single scale.
+    # * Sample Model: nm-testing/Phi-3-mini-128k-instruct-FP8
+    unfused_module_in_checkpoint = (weight_scale[-1]
+                                    > torch.finfo(torch.float8_e4m3fn).min)
+
+    # If unfused checkpoint, need requanize with the single scale.
+    if unfused_module_in_checkpoint:
+        start = 0
+        for idx, logical_width in enumerate(logical_widths):
+            end = start + logical_width
+            weight_dq = per_tensor_dequantize(weight[start:end, :],
+                                              weight_scale[idx])
+            # weight[start:end, :], _ = ops.scaled_fp8_quant(
+            #     weight_dq, max_w_scale)
+            weight[start:end, :] = torch.clamp(
+                (weight_dq / max_w_scale),
+                torch.finfo(torch.float8_e4m3fn).min,
+                torch.finfo(torch.float8_e4m3fn).max
+            ).to(torch.float8_e4m3fn)
+            start = end
+
+    return max_w_scale, weight
 
 class Fp8LinearMethod(LinearMethodBase):
     """Linear method for FP8.
@@ -717,14 +750,14 @@ class Fp8LinearMethod(LinearMethodBase):
                                 input_scale, requires_grad=False
                             )
 
-                    weight_scale, weight = requantize_with_max_scale(
+                    weight_scale, weight = requantize_with_max_scale_cpu(
                         weight=weight,
                         weight_scale=weight_scale,
                         logical_widths=layer.logical_widths,
                     )
 
                 # Update layer with new values.
-                layer.weight = Parameter(weight.t(), requires_grad=False)
+                layer.weight = Parameter(weight.t().contiguous(), requires_grad=False)
                 layer.weight_scale = Parameter(weight_scale, requires_grad=False)
                 if (
                     hasattr(self.quant_config, "activation_scheme")
@@ -812,6 +845,22 @@ class Fp8LinearMethod(LinearMethodBase):
                 input_scale=None,
                 bias=bias,
             )
+        if self.quant_config.activation_scheme == "static":
+            q_input = torch.ops.quantized_decomposed.quantize_per_tensor(
+                input=x,
+                scale=layer.input_scale,
+                zero_point=0,
+                quant_min=int(torch.finfo(torch.float8_e4m3fn).min),
+                quant_max=int(torch.finfo(torch.float8_e4m3fn).max),
+                dtype=torch.float8_e4m3fn,
+            )
+            return torch._scaled_mm(
+                q_input,
+                layer.weight,
+                bias = bias,
+                out_dtype=x.dtype,
+                scale_a = layer.input_scale,
+                scale_b = layer.weight_scale,)
 
         return apply_fp8_linear(
             input=x,
