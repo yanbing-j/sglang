06d41f94052c8e94290d0f2dc17376e393665e19
jianan-gu <jianan.gu@intel.com>
2025-12-24 11:11:14 +0800

    minor fix compile on new fp8 ops (#18)

--- python/sglang/srt/layers/quantization/fp8.py
+++ python/sglang/srt/layers/quantization/fp8.py
@@ -2052,7 +2052,7 @@ class Fp8MoEMethod(FusedMoEMethodBase):
                     True,  # is_vnni
                 )
             else:
-                x_q, x_s = torch.ops.sgl_kernel.quantize_fp8e4m3(x, True, None)
+                x_q, x_s = torch.ops.sgl_kernel._quantize_fp8e4m3_vec(x, True, None)
                 output = torch.ops.sgl_kernel.fused_experts_cpu(
                     x_q,
                     layer.w13_weight,
--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -490,6 +490,46 @@ def register_fake_ops():
             N = weight.size(0)
         return input.new_empty(*in_shape[:-1], N, dtype=output_dtype)
 
+    @torch.library.register_fake("sgl_kernel::fp8_scaled_mm_with_quant")
+    def _(
+        input,
+        input_scales,
+        channelwise,
+        weight,
+        weight_scales,
+        bias,
+        output_dtype,
+    ):
+        in_shape = input.shape
+        if weight.dim() == 4:
+            N = weight.size(0) * weight.size(-1)
+        else:
+            N = weight.size(0)
+        return input.new_empty(*in_shape[:-1], N, dtype=output_dtype)
+
+    @torch.library.register_fake("sgl_kernel::_quantize_fp8e4m3_vec")
+    def _(
+        input,
+        channelwise,
+        input_scales,
+    ):
+        M = input.shape[0]
+        K = input.shape[0]
+        act_quant = input.new_empty(M, K, dtype=torch.float8_e4m3fn)
+        scale = input.new_empty(M, dtype=torch.float) if channelwise else input.new_empty(1, dtype=torch.float)
+        return act_quant, scale
+
+    @torch.library.register_fake("sgl_kernel::quantize_fp8e4m3")
+    def _(
+        input,
+        channelwise,
+        input_scales,
+    ):
+        M = input.shape[0]
+        K = input.shape[0]
+        act_quant = input.new_empty(M, K, dtype=torch.float8_e4m3fn)
+        scale = input.new_empty(M, dtype=torch.float) if channelwise else input.new_empty(1, dtype=torch.float)
+        return act_quant, scale
 
 # TODO Remove unnecessary settings for CPUGraphRunner.
 # Re-abstract the graph runner and restructure CPUGraphRunner to reuse the same logic.
