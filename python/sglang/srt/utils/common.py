5f9394af72543cb5d73ae480cb8fbdd875623d0a
Zheng, Beilei <beilei.zheng@intel.com>
2026-02-12 00:58:10 -0800

    cowork with latest torch

--- python/sglang/srt/utils/common.py
+++ python/sglang/srt/utils/common.py
@@ -307,7 +307,10 @@ except:
 try:
     # move torch._C._cpu._is_amx_tile_supported() from cpu_has_amx_support
     # to support torch compile
-    is_amx_tile_supported = torch._C._cpu._is_amx_tile_supported()
+    try:
+        is_amx_tile_supported = torch._C._cpu._is_amx_tile_supported()
+    except:
+        is_amx_tile_supported = torch.cpu._is_amx_tile_supported()
 except:
     is_amx_tile_supported = False
 
