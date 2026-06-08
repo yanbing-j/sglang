4122aeb7491dd1803ae2a70008591466f860b141
YanbingJiang <yanbing.jiang@intel.com>
2026-06-08 15:30:51 +0800

    Add missing args of fused_experts_cpu and qkv_proj_with_rope (#185)

--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -218,6 +218,10 @@ def register_fake_ops(tp_size: int):
         w2_zero,
         a1_scale,
         block_size,
+        w1_bias,
+        w2_bias,
+        alpha,
+        limit,
         is_vnni,
         activation=None,
     ):
@@ -242,6 +246,7 @@ def register_fake_ops(tp_size: int):
         q_a_proj_scale,
         q_b_proj_scale,
         kv_a_proj_scale,
+        w_scale,
         is_vnni,
         block_size,
     ):
