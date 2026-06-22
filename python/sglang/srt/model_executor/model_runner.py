057406cf9568a130e80fb58f6416fe734334e247
Zaili Wang <109502517+ZailiWang@users.noreply.github.com>
2026-06-22 09:35:57 +0800

    Add CBB support (#194)

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -2828,22 +2828,22 @@ class ModelRunner(ModelRunnerKVCacheMixin):
 
     def init_threads_binding(self):
         omp_cpuids = os.environ.get("SGLANG_CPU_OMP_THREADS_BIND", "all")
-        cpu_ids_by_node = get_cpu_ids_by_node()
-        n_numa_node = len(cpu_ids_by_node)
+        cpu_ids_by_node = get_cpu_ids_by_node()[1]
+        n_node = len(cpu_ids_by_node)
         if omp_cpuids == "all":
-            assert self.tp_size <= n_numa_node, (
+            assert self.tp_size <= n_node, (
                 f"SGLANG_CPU_OMP_THREADS_BIND is not set, in this case, "
-                f"tp_size {self.tp_size} should be smaller than or equal to number of numa node on the machine {n_numa_node}. "
-                f"If you need tp_size to be larger than number of numa node, please set the CPU cores for each tp rank via SGLANG_CPU_OMP_THREADS_BIND explicitly. "
+                f"tp_size {self.tp_size} should be smaller than or equal to number of numa/CBB node on the machine {n_node}. "
+                f"If you need tp_size to be larger than number of numa/CBB node, please set the CPU cores for each tp rank via SGLANG_CPU_OMP_THREADS_BIND explicitly. "
                 f"For example, on a machine with 2 numa nodes, where core 0-31 are on numa node 0 and core 32-63 are on numa node 1, "
                 f"it is suggested to use -tp 2 and bind tp rank 0 to core 0-31 and tp rank 1 to core 32-63. "
                 f"This is the default behavior if SGLANG_CPU_OMP_THREADS_BIND is not set and it is the same as setting SGLANG_CPU_OMP_THREADS_BIND=0-31|32-63. "
-                f"If you do need tp_size to be larger than the number of numa nodes, you could set SGLANG_CPU_OMP_THREADS_BIND explicitly for example SGLANG_CPU_OMP_THREADS_BIND=0-15|16-31|32-47|48-63 and run with -tp 4. "
-                f"If you don't want each tp rank to use all the cores on one numa node, you could set for example SGLANG_CPU_OMP_THREADS_BIND=0-15|32-47 and run with -tp 2."
+                f"If you do need tp_size to be larger than the number of numa/CBB nodes, you could set SGLANG_CPU_OMP_THREADS_BIND explicitly for example SGLANG_CPU_OMP_THREADS_BIND=0-15|16-31|32-47|48-63 and run with -tp 4. "
+                f"If you don't want each tp rank to use all the cores on one numa/CBB node, you could set for example SGLANG_CPU_OMP_THREADS_BIND=0-15|32-47 and run with -tp 2."
             )
-            if self.tp_size < n_numa_node:
+            if self.tp_size < n_node:
                 logger.warning(
-                    f"Detected the current machine has {n_numa_node} numa nodes available, but tp_size is set to {self.tp_size}, so only {self.tp_size} numa nodes are used."
+                    f"Detected the current machine has {n_node} numa/CBB nodes available, but tp_size is set to {self.tp_size}, so only {self.tp_size} numa/CBB nodes are used."
                 )
             self.local_omp_cpuid = cpu_ids_by_node[self.tp_rank]
         else:
@@ -2853,9 +2853,9 @@ class ModelRunner(ModelRunnerKVCacheMixin):
                 f"Please double check your settings."
             )
             self.local_omp_cpuid = threads_bind_list[self.tp_rank]
-            if self.tp_size > n_numa_node:
+            if self.tp_size > n_node:
                 logger.warning(
-                    f"TP size ({self.tp_size})is larger than numa node number ({n_numa_node}), "
+                    f"TP size ({self.tp_size})is larger than numa/CBB node number ({n_node}), "
                     f"in this case the available memory amount of each rank cannot be determined in prior. "
                     f"Please set proper `--max-total-tokens` to avoid the out-of-memory error."
                 )
